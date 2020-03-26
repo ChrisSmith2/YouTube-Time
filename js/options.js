4986bc5f24a2f8574c5592efdc1afd2d293a7de8
Chris5407 <christopher.smith4202@gmail.com>
2020-03-26 16:25:53 -0500

    Updated max minutes allowed per day to 1439

--- js/options.js
+++ js/options.js
@@ -7,16 +7,14 @@ $("#saveMinutes").click(function() {
 
 	if (minutes % 0.5 != 0) {
 		minutes = Math.round(minutes*2)/2; //rounds to nearest 0.5
-		$("#minutes").val(minutes);
 	}
 
 	if (minutes < 0) {
-		$("#minutes").val(0);
-		minutes = 0;
-	} else if (minutes > 720) {
-		$("#minutes").val(720);
-		minutes = 720;
+		minutes = 0; 
+	} else if (minutes > 1439) {
+		minutes = 1439;
 	}
+	$("#minutes").val(minutes);
 
 	chrome.storage.local.set({"timeLimit": minutes, "timeLeft": minutes*60}, function() {
 		chrome.runtime.sendMessage({
