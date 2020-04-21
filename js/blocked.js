ea82f24e17a4347a30350aa8f0d8322aaeaa7f88
Chris5407 <christopher.smith4202@gmail.com>
2020-04-20 22:11:56 -0500

    Added google analytics

--- js/blocked.js
+++ js/blocked.js
@@ -1,3 +1,5 @@
+ga('send', 'pageview', '/blocked.html');
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.msg == "goToSavedVideo") {
 		chrome.storage.local.get("savedVideoURL", function(data) {
@@ -9,6 +11,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 $("#override").click(function() {
 	var answer = confirm("Are you sure you need to use YouTube?")
 	if (answer) {
+		ga('send', {hitType: 'event', eventCategory: 'Blocked page', eventAction: 'Override'});
 		chrome.runtime.sendMessage({
 			msg: "override", 
 			value: true
